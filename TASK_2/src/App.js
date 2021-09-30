9346a7e004e81255c4e422bdb6479fdb935441ef
Anshika2021 <77603417+Anshika2021@users.noreply.github.com>
2021-09-30 18:23:12 +0530

    Add files via upload

---
+++ TASK_2/src/App.js
@@ -0,0 +1,51 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavbarComp } from './nav';
+import { CardComp } from './cards';
+import { Container, Row } from 'react-bootstrap';
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App(props) {
+  
+  const [isUser, setIsUser] = useState(false);
+  const [userList, setUserList] = useState([]);
+  const [error, setError] = useState(false)
+  
+  const getUsers = () =>{
+      axios.get('https://reqres.in/api/users?page=1')
+        .then((user) => {
+          setIsUser(true)
+          setUserList(user.data.data)
+        })
+        .catch((error) => {
+          setError(error.response.data)
+        })
+      }
+
+  return (
+    <div className="App">
+    <NavbarComp onClick={getUsers} />
+    <div className={isUser ? "center-area d-none": " center-area"}>
+      <h1>WELCOME TO</h1>
+      <h2>LET'S GROW TASK-2</h2>
+    </div>
+    <Container className={isUser ? "users-container" : "d-none"} style={{marginTop: "5%"}}>
+    <Row>
+      {error ? error : userList.map((user,index) => {
+        return (
+          <CardComp 
+            key={index}
+            imgSrc={user.avatar}
+            firstName={user.first_name}
+            lastName={user.last_name}
+            email={user.email}
+            userId={user.id}
+          />)
+      })}
+    </Row>
+    </Container>
+  </div> );
+}
+
+export default App;
